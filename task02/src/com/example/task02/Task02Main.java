14052f7eeaab75b11ec8ac8729fd932e765550d9
Egoru <egor.rumyancev.9494@mail.ru>
2023-03-26 17:28:57 +0500

    Румянцев Егор. module7

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,17 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+
+        InputStream inputStream = System.in;
+        int prev = inputStream.read();
+        int current = inputStream.read();
+        while (prev != -1) {
+            if ((prev != 13 || current != 10)) {
+                System.out.write(prev);
+            }
+            prev = current;
+            current = inputStream.read();
+        }
+        System.out.flush();
     }
 }
